26829f640118401f029f922d1c6fae8f55df6f9f
Michael Lum <mike@mikelum.com>
2009-07-29 01:10:51 +0000

    moving source to new directory

--- src/org/mondemand/Transport.java
+++
@@ -1,7 +0,0 @@
-package org.mondemand;
-
-public interface Transport {
-	public void sendLogs(String programId, LogMessage[] messages, Context[] contexts) throws TransportException;
-	public void sendStats(String programId, StatsMessage[] messages, Context[] contexts) throws TransportException;
-	public void shutdown() throws TransportException;
-}
